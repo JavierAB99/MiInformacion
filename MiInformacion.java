8ec1f78e9708cd5ffbd681b1d56c690d6e06b707
JavierAB99 <javierarceburgos@gmail.com>
2024-09-16 20:38:13 -0300

    Actividad práctica Mi Informacion

---
+++ MiInformacion.java
@@ -0,0 +1,8 @@
+public class MiInformacion {
+    public static void main(String args[]){
+        System.out.println("Hola, mi nombre es Javier Arce");
+        System.out.println("Tengo 24 años de edad.");
+        System.out.println("Soy de Chile y vivo en la ciudad de Los Angeles");
+        System.out.println("Me gustan mucho los perritos.");
+    }
+}
